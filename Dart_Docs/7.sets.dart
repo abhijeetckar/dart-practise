a10dca25080a24a5a036c28ea7654c75a2c32507
Abhijeet-FNP <ckar.abhijeet@gmail.com>
2021-06-04 02:38:34 +0530

    added practise code

--- Dart_Docs/7.sets.dart
+++ Dart_Docs/7.sets.dart
@@ -1,1 +1,21 @@
 //unordered collection of unique items
+
+void main() {
+  var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
+  //type Set<String>
+
+  var names = <String>{};
+// Set<String> names = {}; // This works, too.
+// var names = {}; // Creates a map, not a set.
+
+  final constantSet = const {
+    'fluorine',
+    'chlorine',
+    'bromine',
+    'iodine',
+    'astatine',
+  };
+// constantSet.add('helium'); // This line will cause an error.
+
+//Sets support spread operators (... and ...?) and collection if and for, just like lists do
+}
